c564fd79583ba361a459bbddd2f7763804c8eaae
Alessandro Sabatelli <planoform@gmail.com>
2025-09-04 00:05:55 -0700

    Style sliders

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,32 @@ import { createTheme } from '@mantine/core';
 export const theme = createTheme({
   fontFamily: 'Oxanium, system-ui, Avenir, Helvetica, Arial, sans-serif',
   headings: { fontFamily: 'Orbitron, sans-serif' },
+  components: {
+    Slider: {
+      styles: {
+        track: {
+          backgroundColor: '#666',
+          height: '2px',
+        },
+        bar: {
+          backgroundColor: '#666',
+          height: '2px',
+        },
+        thumb: {
+          backgroundColor: '#888',
+          border: 'none',
+          width: '16px',
+          height: '16px',
+          '&:hover': {
+            backgroundColor: '#aaa',
+          },
+          '&[data-dragging="true"]': {
+            backgroundColor: '#999',
+          },
+        },
+      },
+    },
+  },
   other: {
     adsrColors: {
       attack: '#51cf66',
